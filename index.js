ee7e83f8382fcdba8e78a531c245e50fb9e4bef2
lauraszyms <lauraszyms@gmail.com>
2018-02-11 10:01:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+const token = 'a26316f8957b46e5bfaf23a90bafcbd80bd54671'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
